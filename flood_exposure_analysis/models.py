9afb141642395e4ae7625dd5a7d895ee9c784e22
joule <joule.louis.buencido@gmail.com>
2026-01-24 13:00:07 +0800

    partial clearing of the project models

--- flood_exposure_analysis/models.py
+++ flood_exposure_analysis/models.py
@@ -1,24 +1,4 @@
-import geocoder
 from django.db import models
 
-mapbox_access_token = 'pk.eyJ1Ijoiam91bGUyMzMxIiwiYSI6ImNtNzMyczF5YTBmdHIybHB3bXVqMWdiaGgifQ.WW8zxem2Mfu8jPk-L_kSdA'
+# Models removed in db-migration reset.
 
-# Create your models here.
-class Address(models.Model):
-    address = models.TextField()
-    lat = models.FloatField(blank=True, null=True)
-    long = models.FloatField(blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if self.address:
-            g = geocoder.mapbox(self.address, key=mapbox_access_token)
-            
-            if g.ok:
-                latlng = g.latlng #returns [lat, long]
-                self.lat = latlng[0]
-                self.long = latlng[1]
-            else:
-                self.lat = None
-                self.long = None
-
-        return super(Address, self).save(*args, **kwargs)
